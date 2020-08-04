67af6a58bf38620ce770d766c4e259389a0c6fc2
Dante <>
2020-08-05 01:22:12 +0700

    * not run yet but commit to save code

--- rename.js
+++ rename.js
@@ -2,6 +2,7 @@ var vorpal = require('vorpal')();
 var fs = require('fs');
 const prompt = require('prompt-sync')();
 var path = require('path');
+const { triggerAsyncId } = require('async_hooks');
 
 vorpal
   .command('contact', 'Reach me at dante.testgame@gmail.com')
@@ -21,41 +22,73 @@ vorpal
     let inputPath = path.dirname(inputPathFile);
     let addedSuffix = fileName.split('').slice(0, fileName.lastIndexOf('.')).join('') + '_' + inputSuffix + fileName.split('').slice(fileName.lastIndexOf('.')).join('');
     //check if file exists or not
+    
     fs.stat(inputPathFile, (error, stats) => {
-      if (error) {
-        this.log("Your file is not found, please try again!");
-        callback();
-      }
-      else {
-        // this.log("Stats object for:" + inputPathFile); 
-        // console.log(stats); 
-        if (!stats.isDirectory) {
-          if (inputSuffix != "") {
-            if (stats.isFile()) {
-              fs.renameSync(inputPath + '/' + fileName, inputPath + '/' + addedSuffix);
-              // Using methods of the Stats object 
-              this.log('File Renamed Successfuly.');
-              this.log("Your file is existed and renamed as: " + addedSuffix);
-              callback();
-            }
-          }
-          else
-            this.log("Your suffix is empty, please try again!");
-            callback();
-        }
-        else
-          this.log("Your input is not a file, please try again!");
-          callback();
-      }
-    });
-    this.log("Input path: " + inputPath);
-    this.log("File name: " + fileName);
-    callback();
-  });
+      // if (error) {
+      //   this.log("Your file is not found, please try again!");
+      //   callback();
+      // }
+      // else {
+      //   // this.log("Stats object for:" + inputPathFile); 
+      //   // console.log(stats); 
+      //   if (!stats.isDirectory) {
+      //     if (inputSuffix != "") {
+      //       if (stats.isFile()) {
+      //         fs.renameSync(inputPath + '/' + fileName, inputPath + '/' + addedSuffix);
+      //         this.log('File Renamed Successfuly.');
+      //         this.log("Your file is existed and renamed as: " + addedSuffix);
+      //         callback();
+      //       }
+      //     }
+      //     else
+      //       this.log("Your suffix is empty, please try again!");
+      //       callback();
+      //   }
+      //   else
+      //     this.log("Your input is not a file, please try again!");
+      //     callback();
+      // }
+      function checkFileExist() {
+        if (stats.isFile())
+          this.log('Your file is found and ready to be renamed!');
+      };
+      
+      function checkFileDirectory() {
+        if (!stats.isDirectory)
+          this.log('Your input is a file!');
+      };
 
-vorpal
-  .delimiter('dante_cli$')
-  .show();
+      function checkSuffix() {
+        if (inputSuffix != "")
+          this.log("Your suffix is not empty");
+      };
+
+      function renameFile(inputPath, fileName, addedSuffix) {
+        fs.renameSync(inputPath + '/' + fileName, inputPath + '/' + addedSuffix);
+        this.log('File Renamed Successfuly.');
+        this.log("Input path: " + inputPath);
+        this.log("File name: " + fileName);
+        this.log("Your file is existed and renamed as: " + addedSuffix);
+      };
+      async function tryRename(){
+        try {
+          await checkFileExist();
+          await checkFileDirectory();
+          await checkSuffix();
+          await renameFile(inputPath, fileName, addedSuffix);
+          callback();
+        }
+        catch (err) {
+          this.log('Something is wrong! Please recheck')
+          callback();
+        };
+      };
+    });
+    tryRename();
+  });
+    vorpal
+      .delimiter('dante_cli$')
+      .show();
 
 // function getFileName (path){
 //   return path.split('\\').pop().split('/').pop();
@@ -63,6 +96,21 @@ vorpal
 
 
 
+// Happy case
+// Given input correct file path
+// Then rename correct
+
+// File not found
+// Given input incorrect file path (missing)
+// Then print friendly error
+
+// Input folder
+// Given input folder path
+// Then print friendly error
+
+// Input empty suffix
+// Given input empy sufix
+// Then print friendly error
 
 
 
