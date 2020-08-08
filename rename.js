957df1769a65dab902524a3d01e00164340be9fd
Dante <>
2020-08-09 00:23:08 +0700

    * version update, (still didn't run yet)

--- rename.js
+++ rename.js
@@ -22,95 +22,60 @@ vorpal
     let inputPath = path.dirname(inputPathFile);
     let addedSuffix = fileName.split('').slice(0, fileName.lastIndexOf('.')).join('') + '_' + inputSuffix + fileName.split('').slice(fileName.lastIndexOf('.')).join('');
     //check if file exists or not
-    
-    fs.stat(inputPathFile, (error, stats) => {
-      // if (error) {
-      //   this.log("Your file is not found, please try again!");
-      //   callback();
-      // }
-      // else {
-      //   // this.log("Stats object for:" + inputPathFile); 
-      //   // console.log(stats); 
-      //   if (!stats.isDirectory) {
-      //     if (inputSuffix != "") {
-      //       if (stats.isFile()) {
-      //         fs.renameSync(inputPath + '/' + fileName, inputPath + '/' + addedSuffix);
-      //         this.log('File Renamed Successfuly.');
-      //         this.log("Your file is existed and renamed as: " + addedSuffix);
-      //         callback();
-      //       }
-      //     }
-      //     else
-      //       this.log("Your suffix is empty, please try again!");
-      //       callback();
-      //   }
-      //   else
-      //     this.log("Your input is not a file, please try again!");
-      //     callback();
-      // }
-      function checkFileExist() {
-        if (stats.isFile())
-          this.log('Your file is found and ready to be renamed!');
-      };
-      
-      function checkFileDirectory() {
-        if (!stats.isDirectory)
-          this.log('Your input is a file!');
-      };
 
-      function checkSuffix() {
-        if (inputSuffix != "")
-          this.log("Your suffix is not empty");
-      };
 
-      function renameFile(inputPath, fileName, addedSuffix) {
-        fs.renameSync(inputPath + '/' + fileName, inputPath + '/' + addedSuffix);
-        this.log('File Renamed Successfuly.');
-        this.log("Input path: " + inputPath);
-        this.log("File name: " + fileName);
-        this.log("Your file is existed and renamed as: " + addedSuffix);
+    function statPromise(path) {
+      return new Promise((resolve, reject) => {
+        fs.stat(path, (err, stats) => {
+          if (err) return reject(err)
+          resolve(stats)
+        })
+      })
+    };
+
+    function checkFileExist(fileStats) {
+      if (fileStats.isFile())
+        this.log('Your file is found and ready to be renamed!');
+    };
+
+    function checkFileDirectory(fileStats) {
+      if (!fileStats.isDirectory)
+        this.log('Your input is a file!');
+    };
+
+    function checkSuffix() {
+      if (inputSuffix != "")
+        this.log("Your suffix is not empty");
+    };
+
+    function renameFile(inputPath, fileName, addedSuffix) {
+      fs.renameSync(inputPath + '/' + fileName, inputPath + '/' + addedSuffix);
+      this.log('File Renamed Successfuly.');
+      this.log("Input path: " + inputPath);
+      this.log("File name: " + fileName);
+      this.log("Your file is existed and renamed as: " + addedSuffix);
+    };
+    async function doRename() {
+      try {
+        await statPromise(inputPathFile);
+        await checkFileExist(statPromise);
+        await checkFileDirectory(statPromise);
+        await checkSuffix();
+        await renameFile(inputPath, fileName, addedSuffix);
+        callback();
+      }
+      catch (err) {
+        this.log('Something is wrong! Please recheck')
+        callback();
       };
-      async function tryRename(){
-        try {
-          await checkFileExist();
-          await checkFileDirectory();
-          await checkSuffix();
-          await renameFile(inputPath, fileName, addedSuffix);
-          callback();
-        }
-        catch (err) {
-          this.log('Something is wrong! Please recheck')
-          callback();
-        };
-      };
-    });
-    tryRename();
+    };
+    doRename();
+
   });
-    vorpal
-      .delimiter('dante_cli$')
-      .show();
+vorpal
+  .delimiter('dante_cli$')
+  .show();
 
 // function getFileName (path){
 //   return path.split('\\').pop().split('/').pop();
 // }
-
-
-
-// Happy case
-// Given input correct file path
-// Then rename correct
-
-// File not found
-// Given input incorrect file path (missing)
-// Then print friendly error
-
-// Input folder
-// Given input folder path
-// Then print friendly error
-
-// Input empty suffix
-// Given input empy sufix
-// Then print friendly error
-
-
-
